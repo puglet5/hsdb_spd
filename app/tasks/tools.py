90820cd8dee9c9c55cd4f0b4e0c540abdaf8ec4c
Michael Basmanov <puglet5@mail.ru>
2023-02-23 01:00:38 +0300

    more typing, error handling

--- app/tasks/tools.py
+++ app/tasks/tools.py
@@ -4,14 +4,29 @@ import requests
 import csv
 import codecs
 import numpy as np
+import numpy.typing as npt
 import pandas as pd
+
 from findpeaks import findpeaks
+from typing import TypedDict
 
 from app.config.settings import settings
 from celery import shared_task
 from ..tasks import communication
 
 URL = str
+
+
+class Spectrum(TypedDict):
+    file_url: str
+    filename: str
+    id: int
+    sample: dict[int, str]
+    format: str
+    status: str
+    category: str
+    range: str
+    metadata: str | dict | None
 
 
 def validate_json(json_data) -> bool:
@@ -24,101 +39,114 @@ def validate_json(json_data) -> bool:
     return True
 
 
-def download_file(url: URL) -> io.BytesIO:
+def download_file(url: URL) -> io.BytesIO | None:
     """
     Download file from given url and return it as an in-memory buffer
     """
     try:
         response = requests.get(url)
-        file: io.BytesIO = io.BytesIO(response.content)
-        file.seek(0)
-        return file
-    except Exception as e:
-        return e  # type: ignore
+    except:
+        return None
+    file: io.BytesIO = io.BytesIO(response.content)
+    file.seek(0)
+    return file
 
 
-def find_peaks(file: io.BytesIO) -> np.ndarray:
+def find_peaks(file: io.BytesIO) -> npt.NDArray | None:
     data = np.loadtxt(file, delimiter=",")[:, 1]
     data = data / np.max(data)
     fp = findpeaks(method='topology', lookahead=2, denoise="bilateral")
     result = fp.fit(data)
-    df: pd.DataFrame = result["df"]  # type: ignore
+    if result:
+        df: pd.DataFrame = result["df"]
+    else:
+        return None
 
-    filtered_pos = df.query('peak == True & rank != 0 & rank < 40 & y >= 0.05')[
+    filtered_pos: npt.NDArray = df.query('peak == True & rank != 0 & rank <= 40 & y >= 0.03')[
         "x"].to_numpy()
     return filtered_pos
 
 
-def convert_dpt(file: io.BytesIO, filename: str) -> io.BytesIO:
+def convert_dpt(file: io.BytesIO, filename: str) -> io.BytesIO | None:
     """
     Convert FTIR .1.dpt and .0.dpt files to .csv
     """
-    csv_data = csv.reader(codecs.iterdecode(file, 'utf-8'))
-    file.flush()
-    file.seek(0)
+    try:
+        csv_data = csv.reader(codecs.iterdecode(file, 'utf-8'))
+        file.flush()
+        file.seek(0)
 
-    sio: io.StringIO = io.StringIO()
+        sio: io.StringIO = io.StringIO()
 
-    writer = csv.writer(sio, dialect='excel', delimiter=',')
-    for row in csv_data:
-        writer.writerow(row)
+        writer = csv.writer(sio, dialect='excel', delimiter=',')
+        for row in csv_data:
+            writer.writerow(row)
 
-    sio.seek(0)
-    bio = io.BytesIO(sio.read().encode('utf8'))
+        sio.seek(0)
+        bio = io.BytesIO(sio.read().encode('utf8'))
 
-    sio.flush()
-    sio.seek(0)
+        sio.flush()
+        sio.seek(0)
 
-    bio.name = f'{filename.rsplit(".", 2)[0]}.csv'
-    bio.seek(0)
+        bio.name = f'{filename.rsplit(".", 2)[0]}.csv'
+        bio.seek(0)
 
-    return bio
+        return bio
+    except:
+        return None
 
 
-def construct_metadata(init, peak_data: np.ndarray) -> dict | None:
+def construct_metadata(init, peak_data: npt.NDArray) -> dict | None:
+    """
+    Construct JSON object from existing spectrum metadata and peak metadata from processing
+    """
     peak_metadata = {"peaks": [{"position": str(i)} for i in peak_data]}
     if isinstance(init, str):
         return {**json.loads(init), **peak_metadata}
     elif isinstance(init, dict):
         return {**init, **peak_metadata}
     else:
-        return
+        return None
 
 
 @shared_task(bind=True, autoretry_for=(Exception,), retry_backoff=True, retry_kwargs={"max_retries": 0},
              name='spectra:process_spectrum')
-def process_spectrum(self, id: int) -> dict:
+def process_spectrum(self, id: int) -> dict[str, str]:
     """
     Process passed spectrum based on its filetype
 
     Filetype is defined in spectrum["format"]. Supported filetypes: .dpt
     """
     hsdb_url: URL = settings.hsdb_url
-    spectrum: dict = json.loads(communication.get_spectrum(id))["spectrum"]
+    spectrum: Spectrum = json.loads(communication.get_spectrum(id))["spectrum"]
     file_url: URL = f'{hsdb_url}{spectrum["file_url"]}'
     filename: str = spectrum["filename"]
 
-    file: io.BytesIO = download_file(file_url)
+    file = download_file(file_url)
+    if file is None:
+        communication.update_status(id, "error")
+        return {"message": f"Error getting spectrum file from server"}
+
     communication.update_status(id, "ongoing")
 
     if spectrum["format"] == "dpt":
-        try:
-            processed_file: io.BytesIO = convert_dpt(file, filename)
-            peak_data: np.ndarray = find_peaks(processed_file)
-        except Exception as e:
+        processed_file = convert_dpt(file, filename)
+        if processed_file is not None:
+            peak_data = find_peaks(processed_file)
+        else:
             communication.update_status(id, "error")
-            return {"message": f"error coverting spectrum with id {id} ({str(e)})"}
+            return {"message": f"Error coverting spectrum with id {id}"}
     else:
         communication.update_status(id, "error")
-        return {"message": f"unsupported filetype for spectrum with id {id}"}
+        return {"message": f"Unsupported filetype for spectrum with id {id}"}
 
     processed_file.seek(0)
 
     communication.patch_with_processed_file(id, processed_file)
 
-    if validate_json(spectrum["metadata"]):
+    if validate_json(spectrum["metadata"]) and peak_data is not None:
         metadata = construct_metadata(spectrum["metadata"], peak_data)
-        if metadata:
+        if metadata is not None:
             communication.update_metadata(id, metadata)
 
     processed_file.flush()
@@ -126,4 +154,4 @@ def process_spectrum(self, id: int) -> dict:
 
     communication.update_status(id, "successful")
     print(validate_json(spectrum["metadata"]))
-    return {"message": f"processed spectrum with id {id}"}
+    return {"message": f"Successfully processed spectrum with id {id}"}
